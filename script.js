62a33c97a6849baaa95e1b119e6ac90fb9bbae99
lateteetlesmains <127963157+lateteetlesmains@users.noreply.github.com>
2024-07-18 15:24:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 function sendColor() {
-    var color = document.getElementById("colorPicker").value;
-            var brightness = document.getElementById("brightness").value;
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "/set_color?color=" + color.substring(1) + "&brightness=" + brightness, true);
-            xhr.send();
-   
+    var colorPicker = document.getElementById("colorPicker");
+    var brightnessInput = document.getElementById("brightness");
+
+    if (colorPicker && brightnessInput) {
+        var color = colorPicker.value;
+        var brightness = brightnessInput.value;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/set_color?color=" + color.substring(1) + "&brightness=" + brightness, true);
+        xhr.send();
+    } else {
+        console.error("Color picker or brightness input not found.");
+    }
 }
 
 function adjustBrightness() {
-     var color = document.getElementById("colorPicker").value;
-            var brightness = document.getElementById("brightness").value;
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "/set_brightness?color=" + color.substring(1) + "&brightness=" + brightness, true);
-            xhr.send();
+    var colorPicker = document.getElementById("colorPicker");
+    var brightnessInput = document.getElementById("brightness");
+
+    if (colorPicker && brightnessInput) {
+        var color = colorPicker.value;
+        var brightness = brightnessInput.value;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/set_brightness?color=" + color.substring(1) + "&brightness=" + brightness, true);
+        xhr.send();
+    } else {
+        console.error("Color picker or brightness input not found.");
+    }
 }
+
